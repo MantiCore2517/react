5eacca2aa2d3e5a2ddd4fa49881b9fb58b763074
MantiCore2517 <107882760+MantiCore2517@users.noreply.github.com>
2025-02-01 21:49:49 +0300

    first finished

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,79 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import "./style.css";
 
 export const App = () => {
-	const [count, setCount] = useState(0);
+	const [value, setValue] = useState(`""`);
+	const [hiddenAtribute, setHiddenAttribute] = useState(true);
+	const [list, setList] = useState([]);
 
-	const getYear = () => {
-		return new Date(Date.now()).getFullYear();
+	const onClick = (event) => {
+		event.preventDefault;
+
+		if (event.target.textContent === "Ввести новое") {
+			const result = prompt("Введите значение");
+			if (result && result.length >= 3) {
+				setHiddenAttribute(true);
+				setValue(`"${result}"`);
+			} else {
+				setHiddenAttribute(false);
+				setValue(`""`);
+			}
+		} else {
+			setList((a) => [
+				{
+					key: Math.random().toString(16).slice(2),
+					value: value,
+				},
+				...a,
+			]);
+			setValue(`""`);
+		}
 	};
 
 	return (
 		<>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-			</div>
+			<div className="app">
+				<h1 className="page-heading">Ввод значения</h1>
 
-			<h2>{getYear()}</h2>
+				<p className="no-margin-text">
+					Текущее значение <code>value</code>: {value}
+				</p>
+
+				<div className="error" hidden={hiddenAtribute}>
+					Введенное значение должно содержать минимум 3 символа
+				</div>
+
+				<div className="buttons-container">
+					<button className="button" onClick={onClick}>
+						Ввести новое
+					</button>
+
+					{value.length >= 3 ? (
+						<button className="button" onClick={onClick}>
+							Добавить в список
+						</button>
+					) : (
+						<button className="button" onClick={onClick} disabled>
+							Добавить в список
+						</button>
+					)}
+				</div>
+				<div className="list-container">
+					<h2 className="list-heading">Список:</h2>
+
+					{list.length === 0 && (
+						<p className="no-margin-text nolist">Нет добавленных элементов</p>
+					)}
+
+					<ul className="list">
+						{list.map(({ key, value }) => (
+							<li className="list-item" key={key}>
+								{value}
+							</li>
+						))}
+					</ul>
+				</div>
+			</div>
 		</>
 	);
 };
